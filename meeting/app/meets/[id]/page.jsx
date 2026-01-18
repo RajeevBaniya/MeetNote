8ce0b342ab64dedcc0bf604ab0936c7d6aee7a9d
RajeevBaniya <baniyarajeev811@gmail.com>
2026-01-19 02:12:47 +0530

    Implemented landing page for joining meeting

---
+++ meeting/app/meets/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MeetingPage = ({ params }) => {
+    return (
+        <div>MeetingPage</div>
+    );
+};
+
+export default MeetingPage;
--- meeting/app/page.js
+++ meeting/app/page.js
@@ -1,7 +1,41 @@
+"use client";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+
+  const handleJoin = () => {
+    const name = username.trim() === "" ? "anonymous" : username.trim();
+
+    // const meetingId = "meeting-" + Math.random().toString(36).substring(2, 10);
+    const meetingId = process.env.NEXT_PUBLIC_CALL_ID;
+
+    router.push(`/meets/${meetingId}?name=${encodeURIComponent(name)}`);
+  };
+
   return (
-    <div>Hello</div>
+    <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
+      <div className="p-8 bg-gray-800/60 rounded-2xl border border-gray-700 w-80 shadow-2xl">
+        <h2 className="text-xl font-semibold mb-4 text-center">
+          Enter your name
+        </h2>
+
+        <input
+          className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray-600 text-white placeholder-gray-400 focus:ring-2 focus:ring-blue-500 outline-none transition"
+          placeholder="e.g. John (optional)"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <button
+          onClick={handleJoin}
+          className="mt-5 w-full py-3 bg-blue-800 hover:bg-blue-900 rounded-lg font-medium shadow-lg transition">
+          Join Meeting
+        </button>
+      </div>
+    </div>
   );
 }
